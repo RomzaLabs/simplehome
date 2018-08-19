9461ec29f261a0088631052c6dc926dd13cff9da
Rommel Rico <me@rommelrico.com>
2018-08-19 09:32:17 -0700

    Adding Last Updated date to GitHub Feed

--- assets/js/githubFeed.js
+++ assets/js/githubFeed.js
@@ -27,13 +27,20 @@ var GithubFeed = {
         };
     },
 
-    bindTemplate: function(name, description, url, language) {
+    bindTemplate: function(name, description, url, language, updated_at) {
         var container = '';
+        var lcLanguage = language.toLowerCase();
+        var date = new Date(updated_at);
+        var options = { month: 'long', day: 'numeric', year: 'numeric' };
+        var formattedDate = date.toLocaleDateString("en-US", options);
+
 
         container += "<article class='col-6 col-12-xsmall work-item'>";
         container += "<h3><a href='" + url + "'>" + name + "</a></h3>";
         container += "<p>" + description + "</p>";
-        container += "<p><span class='" + language +" language-dot'></span> " + language + "</p>";
+        container += "<p><span class='" + lcLanguage +"-dot language-dot'></span> "
+            + "<span class='" + lcLanguage + "-label'>" + language + "</span> "
+            + "| Last Updated: " + formattedDate +" </p>";
         container += "</article>";
 
         return container;
@@ -49,7 +56,13 @@ var GithubFeed = {
                 i;
 
             for(i = 0; i < repos.length; i++) {
-                content += self.bindTemplate(repos[i].name, repos[i].description, repos[i].svn_url, repos[i].language);
+                content += self.bindTemplate(
+                    repos[i].name,
+                    repos[i].description,
+                    repos[i].svn_url,
+                    repos[i].language,
+                    repos[i].updated_at
+                );
             }
 
             // content += '</ul>';
